580785c925d9d416f999e7df834b00992fb12525
zhangjk4859 <573581325@qq.com>
2016-10-28 17:34:35 +0800

    用JS更改导航标题

--- juzi Credit/pages/repay/repayDetail/repayDetail.js
+++ juzi Credit/pages/repay/repayDetail/repayDetail.js
@@ -33,7 +33,9 @@ Page({
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
-    String2
+    wx.setNavigationBarTitle({
+    title: '还款计划详情'
+})
   },
   onReady:function(){
     // 页面渲染完成
