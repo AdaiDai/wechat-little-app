4906fe606efb1ebc77aba69b57016656b7978cf0
zhangjk4859 <573581325@qq.com>
2016-10-28 14:30:45 +0800

    借贷详情界面第一第二部分完成

---
+++ juzi Credit/pages/repay/repayDetail/repayDetail.js
@@ -0,0 +1,39 @@
+Page({
+  data:{
+    info:[
+                {
+                  title:"利息总额",
+                  detail:"300.0"
+                },
+                {
+                  title:"剩余本金",
+                  detail:"10000.0"
+                },
+                {
+                  title:"贷款期限",
+                  detail:"2016-05-16至2016-05-16"
+                }
+              
+              ]
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+    String2
+  },
+  onReady:function(){
+    // 页面渲染完成
+    String3
+  },
+  onShow:function(){
+    // 页面显示
+    String4
+  },
+  onHide:function(){
+    // 页面隐藏
+    String5
+  },
+  onUnload:function(){
+    // 页面关闭
+    String6
+  }
+})
